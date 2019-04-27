3802daf6e3147f322cc5b92ce790c1acd3043695
Collin <collinclyons@gmail.com>
2019-04-27 13:26:24 -0700

    Level Updates
    Added moon terrain & basics of button puzzle script

---
+++ Assets/Scripts/Environment/Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : Interactable
+{
+    public int ButtonID;
+
+    public ButtonPuzzle Puzzle;
+
+    public override void Activate()
+    {
+        base.Activate();
+        Debug.Log("Clicked button " + ButtonID);
+    }
+}
